56585e8e7cb0ef6ab8c722c46ec0c1c4a3eb0253
Aashish Katwal <aashiskutwal5@gmail.com>
2022-04-28 22:49:05 +0545

    Logout feature added. code for login validation from file
    Changed code structure

--- Source/Controller/LoginController.cpp
+++ Source/Controller/LoginController.cpp
@@ -12,74 +12,55 @@ public:
     Address is used so that the memory consumption is low. Creating new object would allocate new memory location each time
     object is created. This way, we pass only the memory location so that no any new memory is allocated.
     */
-    bool isValidUser(User *user) {
-
-         if(user->getUserType().compare("Student") == 0) {
-            //filename = student.dat; //(This will be done later.)
-            ifstream student_login_file("Login_Std.dat", ios::in|ios::app);
-
-        //     //check the username from the file 
-            User user_data;
-         //   Student student_data;
-            int flag = 0;
-
-            if(!student_login_file){
-                cout << "\nFile not found!";
-                exit(1);
-            }
-
-            while(!student_login_file.eof()){
-                student_login_file >> user_data;
-                if(!user_data.get_username().compare(user->get_username()) && !user_data.get_password().compare(user -> get_password()) ){
-                    student_login_file.close();
-                    return true;
-                }
-            }
-            student_login_file.close();
-            return false;
-            
-
-
-        }
-
-        if(user->getUserType().compare("Staff") == 0) {
-
-            ifstream staff_login_file("login_Staff.dat", ios::in);
-
-            User user_data;
-
-            if(!staff_login_file){
-                cout << "\nFile not found!";
-                exit(1);
-            }
-            while(!staff_login_file.eof()){
-                staff_login_file >> user_data;
-                if(!user_data.get_username().compare(user->get_username()) && !user_data.get_password().compare(user ->get_password()) ){
-                    staff_login_file.close();
-                    return true;
-                }
-            }
-            staff_login_file.close();
-            return false;
-
-            
-
-        }
-
-        /*
-        Compares the username and the password with that of the data in file.
-        If matched, the compare() returns 0 else it returns -1. On this basis the login is validated.
-        */
-        if(!user->get_username().compare("User") && !user->get_password().compare("123")) {
-            cout << "\n\tHurray!! Login Successful.\n\n\t";
-            return true;
-        }
-
-        return false;
-        
-    }
+    bool isValidUser(User *user);
 
     void addLoginCredential(string username, string password);
 
 };
 
+
+bool LoginController::isValidUser(User *user) {
+
+    /*
+    Compares the username and the password with that of the data in file.
+    If matched, the compare() returns 0 else it returns -1. On this basis the login is validated.
+    */
+    if(!user->get_username().compare("User") && !user->get_password().compare("123")) {
+        cout << "\n\tHurray!! Login Successful.\n\n\t";
+        return true;
+
+    } else {
+
+        ifstream login_file;
+
+        if(user->getUserType().compare("Student") == 0) {
+            login_file.open("Login_Std.dat", ios::in);
+        } else if(user->getUserType().compare("Staff") == 0) {
+            login_file.open("Login_Staff.dat", ios::in);
+        } else {
+            login_file.open("Login_Admin.dat", ios::in);
+        }
+        
+        //check the username from the file 
+        User user_data;
+        
+        int flag = 0;
+
+        if(login_file){ //If it returns 1, it indicates that the file exists.
+
+            //Checks the validity of login credentails with that in the file.
+            while(!login_file.eof()){
+                login_file >> user_data;
+                if(!user_data.get_username().compare(user->get_username()) && !user_data.get_password().compare(user -> get_password()) ){
+                    login_file.close();
+                    cout << "\n\tHurray!! Login Successful.\n\n\t";
+                    return true;
+                }
+            }
+        }
+
+        login_file.close();
+    }
+    return false;
+        
+}
--- Source/View/Menu.cpp
+++ Source/View/Menu.cpp
@@ -7,181 +7,192 @@
 class Menu {
     int menuChoice;
 public:
+static int isLoggedOut;
 
-    /* Displays the welcome message on top of the screen */
-    void welcome(string loggedInUserType) {
-	    system("cls");
-        cout << "\t School Management System\n";
-        cout << "\t**************************\n\n";
+void welcome(string);
 
-        if(loggedInUserType.length() == 0){
-    	    cout << "\t<== Login ==>\n";	
-	    }else{
-   	        cout << "\t<== " << loggedInUserType << " Panel ==>\n";
-	    }
-    }
+int loginMenu();
 
-    /* lets user select which  */
-    int loginMenu() {
-    	cout << "\n\t1. Student Login";
-        cout << "\n\t2. Staff Login";
-        cout << "\n\t3. Accounts Login";
-        cout << "\n\t0. Exit\n";
-        cout << "\n\tEnter your choice: ";
-        cin >> menuChoice;
-        
-        return menuChoice;
-    }
+bool login(User*);
 
-    //take credentials
-    bool login(User *user) {
-        string temp_data = "";
-        char temp;
+bool mainOptions(User*);
 
-        cout << "\n\tEnter your username: ";
-		cin >> temp_data;
-        user->set_username(temp_data);
-
-        cout << "\tEnter your password: ";
-        /* Accepts Password and displays '*' instead of entered character. */
-		temp_data = "";
-        while(true) {
-            temp = getch();
-            /*Checks if user hit 'Enter'. If yes, the password end is declared by breaking out of the loop. */
-            if(temp == 13) {
-                break;
-            }
-            cout <<"*";
-            temp_data = temp_data + temp;
-        }
-
-        user->set_password(temp_data);
-
-        if(!LoginController().isValidUser(user)) {
-            cout << "\n\tOopsies!! Login Failed.\n\n\t";
-            system("pause");
-            return false;
-        }
-        return true;
-
-    }
-
-    //This is the main option. It contains several options like add, update, delete, search, etc.
-    bool mainOptions(User *user) {
-	    welcome(user->getUserType());
-    
-	    /* Displays main menu of the program */
-	    cout <<"\t1. View Record(s)\n";
-	    cout <<"\t2. Update Record\n";
-        
-        /* Restricting Staffs from viewing marksheet*/
-	    if( user->getUserType().compare("Staff") != 0){
-	    	cout <<"\t3. Show Marksheet\n";
-	    }
-    
-	    /* Only administrative users will be able to access these options. */
-	    if( !user->getUserType().compare("Admin")){
-	    	cout << "\t4. Add Record(s)\n";
-	    	cout << "\t5. Delete Record\n";
-	    	cout << "\t6. Search Record\n";
-	    	cout << "\t7. View Login Information\n";
-	    	cout << "\t8. Save Marks\n";
-	    }
-
-	    cout << "\t9. Change Password\n";
-	    cout << "\t10.Logout\n";
-	    cout << "\t0. Exit\n";
-	    cout << "\n\tEnter your choice: ";
-	    cin >> menuChoice;
-    
-	    /* Prevents Staff members from accessing Administrative Controls. */
-	    if( user->getUserType().compare("Staff") == 0 && menuChoice == 3){
-	    	cout <<"\n\tInvalid input...\n\t";
-	    	system("pause");
-	    	system("cls");
-            return false;
-            
-	    }else if( (!user->getUserType().compare("Student") || !user->getUserType().compare("Staff")) && (menuChoice >= 3 && menuChoice < 10)){
-            /* Preventing Students and Staffs from accessing Administrative Controls. */
-	    	cout <<"\n\tInvalid input...\n\t";
-	    	system("pause");
-	    	system("cls");
-            return false;
-	    }
-
-        return performRequestionOperation(user->getUserType());
-
-    }
-
-// This method will call the respective method from any other class which implements the functionality of the requested operation.
-    bool performRequestionOperation(string userType) {
-		char resp;
-        switch(menuChoice){
-		case 1:
-			if(!userType.compare("Admin") ) {
-                cout << "view admin record";
-				// viewGeneralRecord();
-			} else {
-                cout << "view student/staff record";
-				// getLoggedUserInfo();
-			}
-			break;
-		case 2:
-			if(!userType.compare("Admin") ){
-                cout << "udpate admin record";
-				// updateRecord();
-			} else {
-                cout << "udpate std/student record";
-				// updateSelfRecord();
-			}
-			break;
-		case 3:
-            cout << "marksheet";
-			// displayMarksheet();
-			break;
-		case 4:
-			Registration reg;
-			resp = reg.get_data(userType);
-			system("cls");
-			if ( resp != 'N' && resp != 'n' ) {
-				return performRequestionOperation(userType);
-			}
-			break;
-		case 5:
-            cout << "delete record";
-			// deleteRecord();
-			break;
-		case 6:
-            cout << "search record";
-			// search();
-			break;
-		case 7:
-            cout << "login info";
-			// viewLoginInfo();
-			break;
-		case 8:
-            cout << "save marks";
-			// saveMarks();
-			break;
-		case 9:
-            cout << "change password";
-			// changePassword();
-			break;
-		case 10:
-			cout << "\n\tLogging out...\n\t";
-            //reset the user object through address.
-			system("pause");
-			break;
-		case 0:
-			cout << "\tExiting...";
-			exit(0);
-		default:
-			cout << "\tInvalid choice!!!\n\t";
-			system("pause");
-			return false;
-	    }
-
-        return true;
-    } 
+bool performRequestionOperation(User*);
 
 };
+
+
+/* Displays the welcome message on top of the screen */
+void Menu::welcome(string loggedInUserType) {
+	system("cls");
+    cout << "\t School Management System\n";
+    cout << "\t**************************\n\n";
+    if(loggedInUserType.length() == 0){
+	    cout << "\t<== Login ==>\n";
+	}else{
+        cout << "\t<== " << loggedInUserType << " Panel ==>\n";
+ 	}
+}
+
+/* lets user select which  */
+int Menu::loginMenu() {
+    cout << "\n\t1. Student Login";
+    cout << "\n\t2. Staff Login";
+    cout << "\n\t3. Accounts Login";
+    cout << "\n\t0. Exit\n";
+    cout << "\n\tEnter your choice: ";
+    cin >> menuChoice;
+        
+    return menuChoice;
+}
+
+//take credentials
+bool Menu::login(User *user) {
+    string temp_data = "";
+    char temp;
+
+    cout << "\n\tEnter your username: ";
+	cin >> temp_data;
+    user->set_username(temp_data);
+
+    cout << "\tEnter your password: ";
+    /* Accepts Password and displays '*' instead of entered character. */
+	temp_data = "";
+    while(true) {
+        temp = getch();
+        /*Checks if user hit 'Enter'. If yes, the password end is declared by breaking out of the loop. */
+        if(temp == 13) {
+            break;
+        }
+        cout <<"*";
+        temp_data = temp_data + temp;
+    }
+
+    user->set_password(temp_data);
+
+    if(!LoginController().isValidUser(user)) {
+        cout << "\n\tOopsies!! Login Failed.\n\n\t";
+        system("pause");
+        return false;
+    }
+    return true;
+
+}
+
+//This is the main option. It contains several options like add, update, delete, search, etc.
+bool Menu::mainOptions(User *user) {
+	system("cls");
+	welcome(user->getUserType());
+    
+	/* Displays main menu of the program */
+	cout <<"\t1. View Record(s)\n";
+	cout <<"\t2. Update Record\n";
+        
+    /* Restricting Staffs from viewing marksheet*/
+	if( user->getUserType().compare("Staff") != 0){
+	    cout <<"\t3. Show Marksheet\n";
+	}
+    
+	/* Only administrative users will be able to access these options. */
+	if( !user->getUserType().compare("Admin")){
+	    cout << "\t4. Add Record(s)\n";
+	    cout << "\t5. Delete Record\n";
+	    cout << "\t6. Search Record\n";
+	    cout << "\t7. View Login Information\n";
+	    cout << "\t8. Save Marks\n";
+	}
+
+	cout << "\t9. Change Password\n";
+	cout << "\t10.Logout\n";
+	cout << "\t0. Exit\n";
+	cout << "\n\tEnter your choice: ";
+	cin >> menuChoice;
+    
+	/* Prevents Staff members from accessing Administrative Controls. */
+	if( user->getUserType().compare("Staff") == 0 && menuChoice == 3){
+	    cout <<"\n\tInvalid input...\n\t";
+	    system("pause");
+	    system("cls");
+        return false;
+            
+	}else if( (!user->getUserType().compare("Student") || !user->getUserType().compare("Staff")) && (menuChoice > 3 && menuChoice < 10)){
+        /* Preventing Students and Staffs from accessing Administrative Controls. */
+	    cout <<"\n\tInvalid input...\n\t";
+	    system("pause");
+	    system("cls");
+        return false;
+	}
+
+    return performRequestionOperation(user);
+}
+
+// This method will call the respective method from any other class which implements the functionality of the requested operation.
+bool Menu::performRequestionOperation(User *user) {
+	char resp;
+    switch(menuChoice){
+	case 1:
+		if(!user->getUserType().compare("Admin") ) {
+            cout << "view admin record";
+			// viewGeneralRecord();
+		} else {
+            cout << "view student/staff record";
+			// getLoggedUserInfo();
+		}
+		break;
+	case 2:
+		if(!user->getUserType().compare("Admin") ){
+            cout << "udpate admin record";
+			// updateRecord();
+		} else {
+            cout << "udpate std/student record";
+			// updateSelfRecord();
+		}
+		break;
+	case 3:
+        cout << "marksheet";
+		// displayMarksheet();
+		break;
+	case 4:
+		Registration reg;
+		resp = reg.get_data(user->getUserType());
+		system("cls");
+		if ( resp != 'N' && resp != 'n' ) {
+			return performRequestionOperation(user);
+		}
+		break;
+	case 5:
+        cout << "delete record";
+		// deleteRecord();
+		break;
+	case 6:
+        cout << "search record";
+		// search();
+		break;
+	case 7:
+        cout << "login info";
+		// viewLoginInfo();
+		break;
+	case 8:
+        cout << "save marks";
+		// saveMarks();
+		break;
+	case 9:
+        cout << "change password";
+		// changePassword();
+		break;
+	case 10:
+		cout << "\n\tLogged out successfully...\n\t";
+        user = nullptr;
+		system("pause");
+		isLoggedOut = 1;
+		return false;
+	case 0:
+		cout << "\tExiting...";
+		exit(0);
+	default:
+		cout << "\tInvalid choice!!!\n\t";
+		system("pause");
+		return false;
+	}
+    return true;
+}
